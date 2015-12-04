122a8b9680ce6196816b7d273e83bc90e283b841
Administrator <Administrator@P-DESIGN>
2015-12-04 18:55:55 +0100

    -- Subida de una primera versión de como puede funcionar las listas para seleccionar las canciones

--- buscador_listas/view/index.php
+++ buscador_listas/view/index.php
@@ -15,6 +15,7 @@ include('../model/consulta_buscar_cancion.php');
 // Pie HTML de la página
 processar_template_noentities('buscar_cancion_footer.html',$diccionario);
 ?>
-<!-- <script type="text/javascript" > eval(document.getElementById("runscript").innerHTML ); </script>   
---> </body>
+<script type="text/javascript" > eval(document.getElementById("runscript").innerHTML ); </script>
+<!--<script type="text/javascript" > run(); </script>-->
+ </body>
 </html>
